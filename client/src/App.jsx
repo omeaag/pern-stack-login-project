5bbeda4dbd83e7089b85840569ddec4b82054b13
can <omercansoyu@gmail.com>
2023-05-19 21:03:57 +0300

    started client side

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useState, Fragment } from "react";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Dashboard from "./components/Dashboard";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+  return (
+    <Fragment>
+      <Router>
+        <div className="container">
+          <Switch></Switch>
+        </div>
+      </Router>
+    </Fragment>
+  );
+}
+
+export default App;
