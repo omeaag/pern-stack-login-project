c32b66dd2565d5231e44ee8518e3d7229ff87371
can <omercansoyu@gmail.com>
2023-06-06 20:10:20 +0300

    builded Dashboard component

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,40 @@
+import { useState, useEffect } from "react";
 import React from "react";
 
 const Dashboard = ({ setAuth }) => {
+  const [name, setName] = useState("");
+
+  async function getName() {
+    try {
+      const response = await fetch("http://localhost:5000/dashboard/", {
+        method: "GET",
+        headers: { token: localStorage.token },
+      });
+
+      const parseRes = await response.json();
+
+      setName(parseRes.user_name);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  const logout = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token");
+    setAuth(false);
+  };
+
+  useEffect(() => {
+    getName();
+  }, []);
+
   return (
-    <div className="dashboard">
-      <h1>DASHBOARD</h1>
-      <button onClick={() => setAuth(false)}> log out</button>
+    <div className="container">
+      <h1>Hoş geldin {name}</h1>
+      <button className="btn btn-primary" onClick={(e) => logout(e)}>
+        Logout
+      </button>
     </div>
   );
 };
